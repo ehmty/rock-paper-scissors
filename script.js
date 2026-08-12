07f7fa953024f054237aa9912f2da958239e7afa
ehmty <187899338+ehmty@users.noreply.github.com>
2026-08-12 21:32:53 +0200

    add func to generate randomly the computers choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function getComputerChoice () {
+    const randomNumber = Math.random();
+    let randomChoice;
+
+    if (randomNumber >= 0 && randomNumber < 0.33) {
+        randomChoice = "rock";
+    } else if (randomNumber >= 0.33 && randomNumber < 0.66) {
+        randomChoice = "paper";
+    } else 
+        randomChoice = "scissors";
+   
+    return randomChoice;
+};
+
+console.log(getComputerChoice());
